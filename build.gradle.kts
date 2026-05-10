8b95de3934bdd202859589ce9ddbed7ae43ce6c0
Peter Svensson <petersv@psv.nu>
2026-05-10 12:07:55 +0200

    support later java versions

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
     id("java")
     id("application")
-    id("com.github.johnrengelman.shadow") version "7.1.2"
+    id("com.gradleup.shadow") version "9.4.1"
 }
 
 group = "me.amberichu.headextractor"
